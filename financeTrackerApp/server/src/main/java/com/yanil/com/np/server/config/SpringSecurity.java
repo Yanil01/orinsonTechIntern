4b884fe6d854c036ee815d1972e53cdff04ccdd0
yanil01 <idoyanil45@gmail.com>
2026-06-28 17:06:52 +0545

    basic spring security configuration

---
+++ financeTrackerApp/server/src/main/java/com/yanil/com/np/server/config/SpringSecurity.java
@@ -0,0 +1,25 @@
+package com.yanil.com.np.server.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurity {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(Customizer.withDefaults())
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/register","/health-check").permitAll()
+                        .anyRequest().authenticated()
+                );
+
+        return http.build();
+    }
+}
--- financeTrackerApp/server/src/main/java/com/yanil/com/np/server/controller/ExpenseEntryController.java
+++ financeTrackerApp/server/src/main/java/com/yanil/com/np/server/controller/ExpenseEntryController.java
@@ -10,8 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.math.BigDecimal;
-import java.time.LocalDate;
+
 import java.util.List;
 
 @RestController
